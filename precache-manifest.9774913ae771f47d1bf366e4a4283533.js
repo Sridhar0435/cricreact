eeb38ee6e4f13affcd850c303f9351eef1aa7142
Sridhar0435 <ssri668@gmail.com>
2020-04-06 14:02:47 +0530

    Updates

---
+++ precache-manifest.9774913ae771f47d1bf366e4a4283533.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97792512cc2c54637226566373d8f9ab",
+    "url": "/cricreact/index.html"
+  },
+  {
+    "revision": "c9e9bb4b5ae419f15a91",
+    "url": "/cricreact/static/css/2.b13d8222.chunk.css"
+  },
+  {
+    "revision": "34743e29e75f9384a17f",
+    "url": "/cricreact/static/css/main.22c14ce6.chunk.css"
+  },
+  {
+    "revision": "c9e9bb4b5ae419f15a91",
+    "url": "/cricreact/static/js/2.fd20253c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/cricreact/static/js/2.fd20253c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34743e29e75f9384a17f",
+    "url": "/cricreact/static/js/main.47b69c81.chunk.js"
+  },
+  {
+    "revision": "59743c13060e79d6ed91",
+    "url": "/cricreact/static/js/runtime-main.50d90f31.js"
+  },
+  {
+    "revision": "1d7626e6cbbdc02be15b16fa36add9ae",
+    "url": "/cricreact/static/media/loader.1d7626e6.gif"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/cricreact/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/cricreact/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/cricreact/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/cricreact/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6170e5f910a9b7bc0b5bd19c0fe0dd9e.js"
+  "/cricreact/precache-manifest.9774913ae771f47d1bf366e4a4283533.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cricreact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
